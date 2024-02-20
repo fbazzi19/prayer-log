d48cf60733a2f13aa9d35c49b1f00bbf317d0b82
fbazzi19 <fbazzi19@gmail.com>
2024-02-20 10:25:19 +0100

    Asks if new user and creates an empty csv file if yes

--- prayer_tracker.py
+++ prayer_tracker.py
@@ -1,4 +1,5 @@
 import numpy as np
+import csv
 
 def streak_counter():
     #variables to keep track of streaks
@@ -15,11 +16,25 @@ def streak_counter():
 
 def main():
 
-    #Boolean indicating menstruation status
-    cycle=False
+    #Boolean indicating
+    cycle=False #menstruation status
+    #new_user=True #if the user is new
+
+    #ask if new user
+    is_new=input("Welcome! Is this your first time logging? (y/n)")
+    if is_new=="n":
+        new_user=False #temp
+        #to do: ask for the users file of previous tracking and read (read csv function)
+    elif is_new=="y":
+        #to do: create a new csv file to start tracking
+        a=0 #temp
+        with open('my_prayer_data.csv', 'a', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            #writer.writerow([0,1,2])
+            f.close()
+
     #array containing prayer status: 0=not completed, 1=completed, 2=menstruating
     prayer=np.empty([1,5]) #will likely have to read and write to a csv to monitor data values
-    print(prayer)
 
 if __name__ == '__main__':
     main()
