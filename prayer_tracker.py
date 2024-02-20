36d295e2e2a5a07d781df3d3b52da203b70ff2f0
fbazzi19 <fbazzi19@gmail.com>
2024-02-20 10:46:46 +0100

    Allows the user to input a file name

--- prayer_tracker.py
+++ prayer_tracker.py
@@ -1,3 +1,4 @@
+#potential future development: add date and time tracker to allow user to log multiple times a day for different prayers within one day
 import numpy as np
 import csv
 
@@ -11,7 +12,9 @@ def streak_counter():
     day_streak=0
     #streak calculation: difference in array position between the current entry (today) and the last day with a recorded zero
 
-
+def log_today(file_name):
+    a=0 #temp
+    #to do: ask about today
 
 
 def main():
@@ -19,19 +22,22 @@ def main():
     #Boolean indicating
     cycle=False #menstruation status
     #new_user=True #if the user is new
+    
+    file_name="" #name of the file containing log
 
     #ask if new user
-    is_new=input("Welcome! Is this your first time logging? (y/n)")
+    is_new=input("Welcome! Is this your first time logging? (y/n) ")
     if is_new=="n":
-        new_user=False #temp
-        #to do: ask for the users file of previous tracking and read (read csv function)
+        file_name=input("What is your log's name? ")
+        #to do: add something to check if the file name is valid
+        log_today(file_name)
     elif is_new=="y":
-        #to do: create a new csv file to start tracking
-        a=0 #temp
-        with open('my_prayer_data.csv', 'a', encoding='UTF8') as f:
+        file_name=input("What would you like to name your log? (No spaces or special characters) ")
+        #to do: add something to check if the file name is valid
+        with open(file_name+'.csv', 'a', encoding='UTF8') as f:
             writer = csv.writer(f)
-            #writer.writerow([0,1,2])
             f.close()
+        log_today(file_name)
 
     #array containing prayer status: 0=not completed, 1=completed, 2=menstruating
     prayer=np.empty([1,5]) #will likely have to read and write to a csv to monitor data values
